fdd2a18c5c0e74fed3a26dd8b24b51d8babb533d
Bojan Peric <bojan.peric@ev-box.com>
2021-03-07 20:50:03 +0100

    Initial commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Bojan Peric on 7.3.21..
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code History/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by Bojan Peric on 7.3.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
