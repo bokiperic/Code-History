378cb8736397f472593a835fc220a1f36030dc83
Bojan Peric <bojan.peric@ev-box.com>
2021-03-13 22:39:02 +0100

    Creating Question screen initial UI.
    Introducing Model layer and some refactoring.
    
    Refactoring HStack with ForEach
    
    Background color changes depending on the answer, @State wraper
    
    Creating initial UI, introducing Model layer, refactoring HSTack and adding background color for answers. Also practicing squash.

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let question = Question(
+        questionText: "What was the first computer bug?",
+        possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
+        correctAnswerIndex: 2)
+    
+    
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    .padding()
+                Text(question.questionText)
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                Spacer()
+                HStack {
+                    ForEach(0..<question.possibleAnswers.count) { answerIndex in
+                        Button(action: {
+                            print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                        }, label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                        })
+                    }
+                }
+            }
+            .foregroundColor(.white)
+        }
     }
 }
 
---
+++ Code History/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Bojan Peric on 13.3.21..
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+}
